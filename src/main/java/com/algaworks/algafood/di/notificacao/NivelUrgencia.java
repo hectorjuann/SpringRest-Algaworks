f036bcc10ac2bc272c54a69d3904b96d8970cc30
Hector Juann <juannhector@gmail.com>
2021-10-17 19:31:56 -0300

    Construct e Destroy de Beans

--- src/main/java/com/algaworks/algafood/di/notificacao/NivelUrgencia.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NivelUrgencia.java
@@ -3,6 +3,6 @@ package com.algaworks.algafood.di.notificacao;
 public enum NivelUrgencia {
 
 	URGENTE,
-	NORMAL
+	SEM_URGENCIA
 	
 }
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.di.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -8,13 +11,23 @@ import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
-@Component
+//@Component
 public class AtivacaoClienteService {
 
-	@TipoDoNotificador(NivelUrgencia.NORMAL)
+	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired(required = false)
 	private Notificador notificador;
 
+//	@PostConstruct
+	public void init() {
+		System.out.println("INIT");
+	}
+	
+//	@PreDestroy
+	public void destroy() {
+		System.out.println("DESTROY");
+	}
+	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
