1dbdfab3e00f30488ec78c6a89f748ff11e69434
Hector Juann <juannhector@gmail.com>
2021-10-17 18:38:39 -0300

    ambiguidade de beans

---
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+@Component
+public class NotificadorEmail implements Notificador {
+	
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("Notificando %s atrav�s do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
+	}
+	
+}
---
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.di.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.Notificador;
+
+@Component
+public class AtivacaoClienteService {
+
+	@Autowired(required = false)
+	private List<Notificador> notificadores;
+
+	public void ativar(Cliente cliente) {
+		cliente.ativar();
+
+		for (Notificador notificador : notificadores) {
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo");	
+		}
+		
+	}
+
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+
+}
