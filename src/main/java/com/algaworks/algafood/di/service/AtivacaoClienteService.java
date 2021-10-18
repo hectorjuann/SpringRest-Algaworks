7136f2cac2c2e21b18f0aa66dc5c8c22e816a3ad
Hector Juann <juannhector@gmail.com>
2021-10-17 21:10:12 -0300

    Eventos

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,43 +1,22 @@
 package com.algaworks.algafood.di.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
-//@Component
+@Component
 public class AtivacaoClienteService {
 
-	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
-	@Autowired(required = false)
-	private Notificador notificador;
+	@Autowired
+	private ApplicationEventPublisher eventPublisher;
 
-//	@PostConstruct
-	public void init() {
-		System.out.println("INIT");
-	}
-	
-//	@PreDestroy
-	public void destroy() {
-		System.out.println("DESTROY");
-	}
-	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+		eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 
 	}
-
-//	@Autowired
-//	public void setNotificador(Notificador notificador) {
-//		this.notificador = notificador;
-//	}
 
 }
