32533b2e8cdf3660538cceb041b498693b56b50d
Hector Juann <juannhector@gmail.com>
2021-10-18 13:01:52 -0300

    Create class Cozinha and Restaurante

---
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,57 @@
+package com.algaworks.algafood.domain.model;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Restaurante {
+
+	@Id
+	private Long id;
+	private String nome;
+	
+	@Column(name = "taxa_frete")
+	private BigDecimal taxafrete;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getTaxafrete() {
+		return taxafrete;
+	}
+	public void setTaxafrete(BigDecimal taxafrete) {
+		this.taxafrete = taxafrete;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Restaurante other = (Restaurante) obj;
+		return Objects.equals(id, other.id);
+	}	
+	
+	
+	
+}
